fff2afbf3f2a9811f3a26e76fb746a7c674bee1d
TerebinovSergey <neznacomec94@gmail.com>
2024-07-06 01:03:15 +0400

    feat: set up prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,19 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'react-refresh',
+    'prettier',
+  ],
   rules: {
+    'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
